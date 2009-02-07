e76c66f5c19621f3f46b36997d16a904becfb117
fpeijnenburg <fpeijnenburg@57cffa9e-d0df-11dd-9aad-3f7dfcff7c77>
2009-02-07 22:47:54 +0000

    Added TeamNeedToBecome in add extra team(optional): It's like you have to be CP to become chief, but then you set what you have to be to become that custom team

--- trunk/DarkRP/gamemode/shared.lua
+++ trunk/DarkRP/gamemode/shared.lua
@@ -24,7 +24,7 @@ team.SetUp(TEAM_CHIEF, "Civil Protection Chief", Color(20, 20, 255, 255))
 //team.SetUp(TEAM_HOBO, "Hobo", Color(80, 45, 0, 255))
 
 RPExtraTeams = {}
-function AddExtraTeam( Name, color, model, Description, Weapons, command, maximum_amount_of_this_class, Salary, admin, Vote)
+function AddExtraTeam( Name, color, model, Description, Weapons, command, maximum_amount_of_this_class, Salary, admin, Vote, NeedToChangeFrom)
 	if not Name or not color or not model or not Description or not Weapons or not command or not maximum_amount_of_this_class or not Salary or not admin or Vote == nil then
 		local text = "One of the custom teams is wrongly made! Attempt to give name of the wrongly made team!(if it's nil then I failed):\n" .. tostring(Name)
 		print(text)
@@ -32,7 +32,7 @@ function AddExtraTeam( Name, color, model, Description, Weapons, command, maximu
 			if ply:IsAdmin() then ply:ChatPrint("WARNING: "..text) end
 		end)	
 	end
-	table.insert(RPExtraTeams, {name = Name, model = model, Des = Description, Weapons = Weapons, command = command, max = maximum_amount_of_this_class, salary = Salary, admin = admin or 0, Vote = tobool(Vote)})
+	table.insert(RPExtraTeams, {name = Name, model = model, Des = Description, Weapons = Weapons, command = command, max = maximum_amount_of_this_class, salary = Salary, admin = admin or 0, Vote = tobool(Vote), NeedToChangeFrom = NeedToChangeFrom})
 	team.SetUp(9 + #RPExtraTeams, Name, color)
 	local Team = 9 + #RPExtraTeams
 	--[[ if SERVER then
