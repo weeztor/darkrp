fad422fdffc98981542e691e29efa674ce1daedc
fpeijnenburg <fpeijnenburg@57cffa9e-d0df-11dd-9aad-3f7dfcff7c77>
2009-02-25 13:34:19 +0000

    [CHANGE] Cooks can now buy food too when hungermod is off [ADD] Eating food makes the TF2 nom sound(bad luck if you don't have TF2)
    Added pet sounds and I fixed the pet class up a bit

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -60,4 +60,5 @@ outside someone else's door]], {"weapon_bugbait"}, "hobo", 5, 0, 0, false)
 // HAVING "/dude" IN THE COMMAND FIELD IS WROOOOOOOONG!!!!
 //ADD TEAMS UNDER THIS LINE:
 TEAM_PET = AddExtraTeam("Pet", Color(255, 200, 0, 255), "models/player/renamon_b5.mdl", [[Be someone's pet, make sure he treats you well.
-If you don't get treated well, bite him.]], {"bite"}, "pet", 2, 0, 0, false)
+If you don't get treated well, bite him.
+Pet's can't have guns or toolgun(unless you're admin)]], {"bite"}, "pet", 2, 0, 0, false)
