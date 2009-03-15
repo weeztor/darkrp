4a478ad42affa74bd793cefe4e0e2c9ad24d1d50
fpeijnenburg <fpeijnenburg@57cffa9e-d0df-11dd-9aad-3f7dfcff7c77>
2009-03-15 07:37:47 +0000

    Weapon checker can now return weapons to the player. Other cops can give back confisquated weapons too
    Fixed custom teams with custom commands containing uppercase letters not working

--- gamemode/chat.lua
+++ gamemode/chat.lua
@@ -8,7 +8,7 @@ function GM:PlayerSay(ply, text)
 	self.BaseClass:PlayerSay(ply, text)
 
 	for k, v in pairs(ChatCommands) do
-		if v.cmd == string.Explode(" ", string.lower(text))[1] then
+		if string.lower(v.cmd) == string.Explode(" ", string.lower(text))[1] then
 			return v.callback(ply, "" .. string.sub(text, string.len(v.cmd) + 2, string.len(text)))
 		end
 	end
