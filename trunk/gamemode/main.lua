914bb8731a31cba8bd44f7a89783065405de40f4
fpeijnenburg <fpeijnenburg@57cffa9e-d0df-11dd-9aad-3f7dfcff7c77>
2009-04-12 07:43:49 +0000

    [ADD] Message when trying to spawn a banned prop

--- trunk/gamemode/main.lua
+++ trunk/gamemode/main.lua
@@ -2097,7 +2097,7 @@ function GM:PlayerSpawnProp(ply, model)
 	-- Banned props take precedence over allowed props
 	if CfgVars["banprops"] == 1 then
 		for k, v in pairs(BannedProps) do
-			if v == model then return false end
+			if v == model then Notify(ply, 1, 4, "Cannot spawn this prop because it is banned") return false end
 		end
 	end
 
