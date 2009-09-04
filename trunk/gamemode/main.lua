f20c72283d5c25cfc5c37bdfa1a8c1653cb3fa3d
fpeijnenburg <fpeijnenburg@57cffa9e-d0df-11dd-9aad-3f7dfcff7c77>
2009-09-04 08:19:02 +0000

    [ADD] Signature to the letters/mails

--- trunk/gamemode/main.lua
+++ trunk/gamemode/main.lua
@@ -735,7 +735,7 @@ local function MakeLetter(ply, args, type)
 	-- Instruct the player's letter window to open
 
 	local ftext = string.gsub(args, "//", "\n")
-	ftext = string.gsub(args, "\\n", "\n")
+	ftext = string.gsub(args, "\\n", "\n") .. "\n\nYours,\n"..ply:Nick()
 	local length = string.len(ftext)
 
 	local numParts = math.floor(length / 39) + 1
