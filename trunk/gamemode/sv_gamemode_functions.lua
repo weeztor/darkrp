2635ac994b8f09bf76d1b82eeede7bc1fa258591
fpeijnenburg <fpeijnenburg@57cffa9e-d0df-11dd-9aad-3f7dfcff7c77>
2010-06-17 09:53:04 +0000

    Fixed incompatability with durgz and possibly other mods.

--- trunk/gamemode/sv_gamemode_functions.lua
+++ trunk/gamemode/sv_gamemode_functions.lua
@@ -336,7 +336,7 @@ function GM:PlayerCanPickupWeapon(ply, weapon)
 	if RPArrestedPlayers[ply:SteamID()] then return false end
 	if ply:IsAdmin() and GetConVarNumber("AdminsSpawnWithCopWeapons") == 1 then return true end
 	if GetConVarNumber("license") == 1 and not ply.DarkRPVars.HasGunlicense and not ply:GetTable().RPLicenseSpawn then
-		if GetConVarNumber("licenseweapon_"..string.lower(weapon:GetClass())) == 1 then
+		if GetConVarNumber("licenseweapon_"..string.lower(weapon:GetClass())) == 1 or not weapon:IsWeapon() then
 			return true
 		end
 		return false
