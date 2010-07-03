7ac486daf65657721a2b78ade517c252446bc499
fpeijnenburg <fpeijnenburg@57cffa9e-d0df-11dd-9aad-3f7dfcff7c77>
2010-07-03 18:26:22 +0000

    minor update.

--- entities/entities/spawned_shipment/cl_init.lua
+++ entities/entities/spawned_shipment/cl_init.lua
@@ -12,11 +12,11 @@ function ENT:Draw()
 	contents = contents.name
 	
 	surface.SetFont("HUDNumber5")
-	local TextWidth = surface.GetTextSize("Weapon:")
+	local TextWidth = surface.GetTextSize("Contents:")
 	local TextWidth2 = surface.GetTextSize(contents)
 	
 	cam.Start3D2D(Pos + Ang:Up() * 25, Ang, 0.2)
-		draw.WordBox(2, -TextWidth*0.5 + 5, -30, "Weapon:", "HUDNumber5", Color(140, 0, 0, 100), Color(255,255,255,255))
+		draw.WordBox(2, -TextWidth*0.5 + 5, -30, "Contents:", "HUDNumber5", Color(140, 0, 0, 100), Color(255,255,255,255))
 		draw.WordBox(2, -TextWidth2*0.5 + 5, 18, contents, "HUDNumber5", Color(140, 0, 0, 100), Color(255,255,255,255))
 	cam.End3D2D()
 	
