38d886853546e1688bc41e9f881ea357641f4976
fpeijnenburg <fpeijnenburg@57cffa9e-d0df-11dd-9aad-3f7dfcff7c77>
2011-03-28 14:42:40 +0000

    Fixed menu

--- trunk/gamemode/DRPDermaSkin.lua
+++ trunk/gamemode/DRPDermaSkin.lua
@@ -7,7 +7,7 @@ skin.DermaVersion       = 1
 
 
 -- Collapsible catagory header
-skin.colCollapsibleCategory = Color(GetConVarNumber("salary1r"), GetConVarNumber("salary1g"), GetConVarNumber("salary1b"), GetConVarNumber("salary1a"))
+skin.colCollapsibleCategory = Color(GetConVarNumber("salary11"), GetConVarNumber("salary12"), GetConVarNumber("salary13"), GetConVarNumber("salary14"))
 skin.fontCategoryHeader = "TargetID"
 
 -- tab
@@ -18,10 +18,10 @@ skin.colPropertySheet = skin.colTab
 
 function skin:PaintFrame(frame)
 	local w, h = frame:GetSize()
-	local color = Color(GetConVarNumber("backgroundr"), GetConVarNumber("backgroundg"), GetConVarNumber("backgroundb"), GetConVarNumber("backgrounda"))//self.bg_color
+	local color = Color(GetConVarNumber("background1"), GetConVarNumber("background2"), GetConVarNumber("background3"), GetConVarNumber("background4"))//self.bg_color
 	draw.RoundedBox(8, 0, 0, w, h, color)
 	
-	surface.SetDrawColor(GetConVarNumber("Healthforegroundr"), GetConVarNumber("Healthforegroundg"), GetConVarNumber("Healthforegroundb"), GetConVarNumber("Healthforegrounda"))
+	surface.SetDrawColor(GetConVarNumber("Healthforeground1"), GetConVarNumber("Healthforeground2"), GetConVarNumber("Healthforeground3"), GetConVarNumber("Healthforeground4"))
 	surface.DrawLine(0, 20, w, 20)
 end
 
@@ -34,8 +34,8 @@ function skin:PaintButton(button)
 	if w <= 32 or h <= 32 then bordersize = 4 end -- This is so small buttons don't look messed up
 	
 	if button.m_bBackground then
-		local color1 = Color(GetConVarNumber("Healthbackgroundr"), GetConVarNumber("Healthbackgroundg"), GetConVarNumber("Healthbackgroundb"), GetConVarNumber("Healthbackgrounda"))
-		local color2 = Color(GetConVarNumber("Healthforegroundr"), GetConVarNumber("Healthforegroundg"), GetConVarNumber("Healthforegroundb"), GetConVarNumber("Healthforegrounda"))
+		local color1 = Color(GetConVarNumber("Healthbackground1"), GetConVarNumber("Healthbackground2"), GetConVarNumber("Healthbackground3"), GetConVarNumber("Healthbackground4"))
+		local color2 = Color(GetConVarNumber("Healthforeground1"), GetConVarNumber("Healthforeground2"), GetConVarNumber("Healthforeground3"), GetConVarNumber("Healthforeground4"))
 		
 		if button:GetDisabled() then
 			color2 = Color(80, 80, 80, 255)
@@ -57,13 +57,13 @@ function skin:PaintOverButton( panel ) end
 
 
 function skin:PaintVScrollBar(scrollbar)
-	local color = Color(GetConVarNumber("Healthbackgroundr"), GetConVarNumber("Healthbackgroundg"), GetConVarNumber("Healthbackgroundb"), GetConVarNumber("Healthbackgrounda"))
+	local color = Color(GetConVarNumber("Healthbackground1"), GetConVarNumber("Healthbackground2"), GetConVarNumber("Healthbackground3"), GetConVarNumber("Healthbackground4"))
 	local w, h = scrollbar:GetSize()
 	draw.RoundedBox(8, 0, 0, w, h, color)
 end
 
 function skin:PaintScrollBarGrip(scrollbar)
-	local color = Color(GetConVarNumber("Healthforegroundr"), GetConVarNumber("Healthforegroundg"), GetConVarNumber("Healthforegroundb"), GetConVarNumber("Healthforegrounda"))
+	local color = Color(GetConVarNumber("Healthforeground1"), GetConVarNumber("Healthforeground2"), GetConVarNumber("Healthforeground3"), GetConVarNumber("Healthforeground4"))
 	local w, h = scrollbar:GetSize()
 	draw.RoundedBox(8, 0, 0, w, h, color)
 end
@@ -71,7 +71,7 @@ end
 function skin:PaintPanelList(panellist)
 	if panellist.m_bBackground then
 		local w, h = panellist:GetSize()
-		local color = Color(GetConVarNumber("Healthbackgroundr"), GetConVarNumber("Healthbackgroundg"), GetConVarNumber("Healthbackgroundb"), GetConVarNumber("Healthbackgrounda"))
+		local color = Color(GetConVarNumber("Healthbackground1"), GetConVarNumber("Healthbackground2"), GetConVarNumber("Healthbackground3"), GetConVarNumber("Healthbackground4"))
 		draw.RoundedBox( 4, 0, 0, w, h, color)
 	end
 end
