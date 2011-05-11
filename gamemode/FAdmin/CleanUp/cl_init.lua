848ce5c9f3eab06d9891c8db2e583a5696796565
fpeijnenburg <fpeijnenburg@57cffa9e-d0df-11dd-9aad-3f7dfcff7c77>
2011-05-11 14:27:36 +0000

    Banana Lord - Made 'stopsounds' use a usermessage & RunConsoleCommand since it's blocked via meta:ConCommand.
    Banana Lord - Added formatNumber function in cl_hud.lua. Used to format the
    wallet to display $12,345 instead of $12345.

--- gamemode/FAdmin/CleanUp/cl_init.lua
+++ gamemode/FAdmin/CleanUp/cl_init.lua
@@ -11,6 +11,10 @@ FAdmin.StartHooks["CleanUp"] = function()
 	FAdmin.ScoreBoard.Server:AddServerAction("Stop all sounds", "FAdmin/icons/CleanUp", Color(155, 0, 0, 255), true, function(ply, button)
 		RunConsoleCommand("_FAdmin", "StopSounds")
 	end)
+
+	usermessage.Hook("FAdmin_StopSounds", function()
+		RunConsoleCommand("stopsounds") -- bypass for ConCommand blocking it
+	end)
 	
 	FAdmin.ScoreBoard.Server:AddServerAction("Clean up server", "FAdmin/icons/CleanUp", Color(155, 0, 0, 255), true, function(ply, button)
 		RunConsoleCommand("_FAdmin", "CleanUp")
