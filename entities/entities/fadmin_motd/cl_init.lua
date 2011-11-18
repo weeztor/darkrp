84490c11929e3f36e4ca96456ef8aeb78aef150a
fpeijnenburg@gmail.com <fpeijnenburg@gmail.com@57cffa9e-d0df-11dd-9aad-3f7dfcff7c77>
2011-11-18 20:09:11 +0000

    Fixed FAdmin motd page size.

--- entities/entities/fadmin_motd/cl_init.lua
+++ entities/entities/fadmin_motd/cl_init.lua
@@ -28,7 +28,7 @@ function ENT:Initialize()
 	self.HTML:SetPos(-512, -256)
 	self.HTML:SetSize(ScrW(), ScrH() - 100)
 	self:LoadPage()
-	
+
 	self.HTML:StartAnimate(100)
 	self.HTML:SetVisible(true)
 end
@@ -47,50 +47,50 @@ local gripTexture = surface.GetTextureID( "sprites/grip" )
 local ArrowTexture = surface.GetTextureID("gui/arrow")
 function ENT:Draw()
 	self.Entity:DrawModel()
-	
+
 	local pos = self.Entity:GetPos()
 	if pos:Distance(LocalPlayer():GetShootPos()) > 300 then return end
-	
+
 	if CurTime() - self.LastDrawn > 0.5 then
 		self.Disabled = true --Disable it again when you stop looking at it
 	end
-	
+
 	self.LastDrawn = CurTime()
 	local IsAdmin = LocalPlayer():IsAdmin()
 	local HasPhysgun = (ValidEntity(LocalPlayer():GetActiveWeapon()) and LocalPlayer():GetActiveWeapon():GetClass() == "weapon_physgun")
-	
+
 	surface.SetFont("ScoreboardText")
 	local TextPosX = surface.GetTextSize("Physgun/use the button to see the MOTD!")*(-0.5)
-	
+
 	local ang = self:GetAngles()
 	ang:RotateAroundAxis(ang:Right(), -90)
 	ang:RotateAroundAxis(ang:Up(), 90)
-	
-	
+
+
 	local posX, posY = WorldToScreen(LocalPlayer():GetEyeTrace().HitPos, self:GetPos() + ang:Up()*3, 0.25, ang)
 	render.SuppressEngineLighting(true)
 	cam.Start3D2D(self:GetPos() + ang:Up()*3, ang, 0.25)
-		
+
 		if self.Disabled then
 			surface.SetDrawColor(0, 0, 0, 255)
 			surface.DrawRect(-512, 256, 1024, -512)
 			surface.SetTextColor( 255, 255, 255, 255 )
-			surface.SetTextPos( TextPosX, 0 ) 
+			surface.SetTextPos( TextPosX, 0 )
 			surface.DrawText("Physgun/use the button to see the MOTD!")
-			
+
 			draw.WordBox(4, -16, 24, "Click!", "default", Color(100, 100, 100, 255), Color(255, 255, 255, 255))
-			
+
 			surface.SetDrawColor(255, 255, 255, 255)
 			if IsAdmin and HasPhysgun then
 				surface.SetTexture(gripTexture)
 				surface.DrawTexturedRect(-10, 240, 16, 16)
 			end
 			if (HasPhysgun and LocalPlayer():KeyDown(IN_ATTACK)) or LocalPlayer():KeyDown(IN_USE) then
-				
+
 				posX, posY = math.Clamp(posX, -506, 506), math.Clamp(posY, -250, 250)
 				surface.SetTexture(ArrowTexture)
 				surface.DrawTexturedRectRotated(posX + 5, posY + 5, 16, 16, 45)
-				
+
 				-- Clicking button
 				if posX > -16 and posX < 16 and posY > 24 and posY < 48 then
 					self:LoadPage()
@@ -101,17 +101,17 @@ function ENT:Draw()
 		elseif self.HTMLMat then
 			surface.SetMaterial(self.HTMLMat)
 			surface.SetDrawColor(255, 255, 255, 255)
-			surface.DrawTexturedRect(-512, -256, 1254, 552)
+			surface.DrawTexturedRect(-512, -256, 1520, 780)
 		end
 	cam.End3D2D()
 	render.SuppressEngineLighting(false)
 	if self.HTMLCloseButton then return end
-	
+
 	--Drawing the actual HTML panel:
-	
+
 	if ((HasPhysgun and LocalPlayer():KeyDown(IN_ATTACK)) or LocalPlayer():KeyDown(IN_USE))
 		and posX > -500 and posX < 500 and posY < 250 and posY > -250 then
-		if not self.Disabled and self.HTML and self.HTML:IsValid() and self.CanClickAgain and CurTime() > self.CanClickAgain then		
+		if not self.Disabled and self.HTML and self.HTML:IsValid() and self.CanClickAgain and CurTime() > self.CanClickAgain then
 			self.CanClickAgain = CurTime() + 1
 			self.HTML:SetPaintedManually(false)
 			self.HTML:SetPos(0, 100)
@@ -127,7 +127,7 @@ function ENT:Draw()
 			self.HTML:RequestFocus()
 			self.HTML:SetKeyBoardInputEnabled(true)
 			self.HTML:MakePopup()
-			
+
 			function self.HTMLCloseButton.DoClick() -- Revert to drawing on the prop
 				self.HTML:SetPos(-512, -256)
 				self.HTML:SetSize(1024, 512)
