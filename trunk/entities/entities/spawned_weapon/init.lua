82f5e25661480c6535384d218b28d7561cb33c41
fpeijnenburg@gmail.com <fpeijnenburg@gmail.com@57cffa9e-d0df-11dd-9aad-3f7dfcff7c77>
2012-02-04 11:56:05 +0000

    God damn bullshit ammo

--- trunk/entities/entities/spawned_weapon/init.lua
+++ trunk/entities/entities/spawned_weapon/init.lua
@@ -32,7 +32,12 @@ function ENT:Use(activator, caller)
 		weapon:SetClip1(self.clip1)
 		weapon:SetClip2(self.clip2 or -1)
 	end
-	activator:GiveAmmo(self.ammo or 0, weapon:GetPrimaryAmmoType())
+	if self.ammo then
+		activator:SetAmmo(self.ammo, weapon:GetPrimaryAmmoType())
+	end
+
+	-- The ammo bullshit gets as bad as having four variables to handle ammo exploits
+	activator:GiveAmmo(self.ammoadd or 0, weapon:GetPrimaryAmmoType())
 
 	self.Entity:Remove()
 end
