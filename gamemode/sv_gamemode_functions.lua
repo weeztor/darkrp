7c6c57d17bece92bb68881a5cb1bd980c2e1f5cb
fpeijnenburg@gmail.com <fpeijnenburg@gmail.com@57cffa9e-d0df-11dd-9aad-3f7dfcff7c77>
2012-02-15 12:35:42 +0000

    reverted sooner loading of MySQL. It didn't work :)

--- gamemode/sv_gamemode_functions.lua
+++ gamemode/sv_gamemode_functions.lua
@@ -732,12 +732,14 @@ function GM:GetFallDamage( ply, flFallSpeed )
 	return 10
 end
 
-function GM:PostGamemodeLoaded()
-	if RP_MySQLConfig and RP_MySQLConfig.EnableMySQL then
-		DB.ConnectToMySQL(RP_MySQLConfig.Host, RP_MySQLConfig.Username, RP_MySQLConfig.Password, RP_MySQLConfig.Database_name, RP_MySQLConfig.Database_port)
-		return
-	end
-	DB.Init()
+function GM:InitPostEntity()
+	timer.Simple(1, function()
+		if RP_MySQLConfig and RP_MySQLConfig.EnableMySQL then
+			DB.ConnectToMySQL(RP_MySQLConfig.Host, RP_MySQLConfig.Username, RP_MySQLConfig.Password, RP_MySQLConfig.Database_name, RP_MySQLConfig.Database_port)
+			return
+		end
+		DB.Init()
+	end)
 end
 
 function GM:PlayerLeaveVehicle(ply, vehicle)
