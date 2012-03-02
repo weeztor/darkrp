f54c1e1e98b59350309263d08a12c5ef5be61929
fpeijnenburg@gmail.com <fpeijnenburg@gmail.com@57cffa9e-d0df-11dd-9aad-3f7dfcff7c77>
2012-03-02 22:34:27 +0000

    merge

--- gamemode/sh_animations.lua
+++ gamemode/sh_animations.lua
@@ -4,63 +4,63 @@ hook.Add("CalcMainActivity", "darkrp_animations", function(ply, velocity) -- Usi
 		ply:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, ACT_ITEM_DROP)
 		ply.anim_DroppingItem = nil
 	end
-	
+
 	-- Giving items!
 	if ply.anim_GivingItem then
 		ply:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, ACT_ITEM_GIVE)
 		ply.anim_GivingItem = nil
 	end
-	
+
 	if CLIENT and ply.SaidHi then
 		ply:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, ACT_SIGNAL_GROUP)
 		ply.SaidHi = nil
 	end
-	
+
 	if CLIENT and ply.ThrewPoop then
 		ply:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, ACT_ITEM_THROW)
 		ply.ThrewPoop = nil
 	end
-	
+
 	if CLIENT and ply.knocking then
 		ply:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, ACT_HL2MP_GESTURE_RANGE_ATTACK_FIST)
 		ply.knocking = nil
 	end
-	
+
 	if CLIENT and ply.usekeys then
 		ply:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, ACT_ITEM_PLACE)
 		ply.usekeys = nil
 	end
-	
+
 	if not SERVER then return end
-	
+
 	-- Hobo throwing poop!
 	local Weapon = ply:GetActiveWeapon()
 	if ply:Team() == TEAM_HOBO and not ply.ThrewPoop and ValidEntity(Weapon) and Weapon:GetClass() == "weapon_bugbait" and ply:KeyDown(IN_ATTACK) then
 		ply.ThrewPoop = true
 		ply:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, ACT_ITEM_THROW)
-		
-		
+
+
 		local RP = RecipientFilter()
 		RP:AddAllPlayers()
-		
-		umsg.Start("anim_throwpoop", RP) 
+
+		umsg.Start("anim_throwpoop", RP)
 			umsg.Entity(ply)
 		umsg.End()
 	elseif ply.ThrewPoop and not ply:KeyDown(IN_ATTACK) then
 		ply.ThrewPoop = nil
 	end
-	
+
 	-- Saying hi/hello to a player
 	if not ply.SaidHi and ValidEntity(Weapon) and Weapon:GetClass() == "weapon_physgun" and ply:KeyDown(IN_ATTACK) then
 		local ent = ply:GetEyeTrace().Entity
 		if ValidEntity(ent) and ent:IsPlayer() then
 			ply.SaidHi = true
 			ply:AnimRestartGesture(GESTURE_SLOT_ATTACK_AND_RELOAD, ACT_SIGNAL_GROUP)
-			
+
 			local RP = RecipientFilter()
 			RP:AddAllPlayers()
-			
-			umsg.Start("anim_sayhi", RP) 
+
+			umsg.Start("anim_sayhi", RP)
 				umsg.Entity(ply)
 			umsg.End()
 		end
@@ -69,10 +69,10 @@ hook.Add("CalcMainActivity", "darkrp_animations", function(ply, velocity) -- Usi
 	end
 end)
 
-if SERVER then 
+if SERVER then
 	local function CustomAnim(ply, cmd, args)
 		local Gesture = tonumber(args[1] or 0)
-		if Gesture < 1782 or Gesture > 1787 then return end
+		if Gesture < 1782 or Gesture > 1900 then return end
 		local RP = RecipientFilter()
 		RP:AddAllPlayers()
 
@@ -88,7 +88,7 @@ end
 local function DropItem(um)
 	local ply = um:ReadEntity()
 	if not ValidEntity(ply) then return end
-	
+
 	ply.anim_DroppingItem = true
 end
 usermessage.Hook("anim_dropitem", DropItem)
@@ -96,7 +96,7 @@ usermessage.Hook("anim_dropitem", DropItem)
 local function GiveItem(um)
 	local ply = um:ReadEntity()
 	if not ValidEntity(ply) then return end
-	
+
 	ply.anim_GivingItem = true
 end
 usermessage.Hook("anim_giveitem", GiveItem)
@@ -104,7 +104,7 @@ usermessage.Hook("anim_giveitem", GiveItem)
 local function ThrowPoop(um)
 	local ply = um:ReadEntity()
 	if not ValidEntity(ply) then return end
-	
+
 	ply.ThrewPoop = true
 end
 usermessage.Hook("anim_throwpoop", ThrowPoop)
@@ -112,7 +112,7 @@ usermessage.Hook("anim_throwpoop", ThrowPoop)
 local function PhysgunHi(um)
 	local ply = um:ReadEntity()
 	if not ValidEntity(ply) then return end
-	
+
 	ply.SaidHi = true
 end
 usermessage.Hook("anim_sayhi", PhysgunHi)
@@ -151,7 +151,7 @@ local function AnimationMenu()
 	function Panel:OnMousePressed()
 		AnimFrame:Close()
 	end
-	
+
 	AnimFrame = AnimFrame or vgui.Create("DFrame", Panel)
 	local Height = table.Count(Anims) * 110
 	AnimFrame:SetSize(200, Height)
