8f8e6dc0a065ecbf52c572c713e5ec7008c06fc4
fpeijnenburg@gmail.com <fpeijnenburg@gmail.com@57cffa9e-d0df-11dd-9aad-3f7dfcff7c77>
2012-06-06 17:21:27 +0000

    Removed FAdmin icon layout. It's shitty and gimmicky.

--- gamemode/FAdmin/CL_Interface/cl_controls.lua
+++ gamemode/FAdmin/CL_Interface/cl_controls.lua
@@ -10,13 +10,13 @@ local PANEL = {}
 
 function PANEL:SizeToContents()
 	local w, h = self.pnlCanvas:GetSize()
-	
+
 	w = math.Clamp(w, ScrW()*0.9, ScrW()*0.9) -- Fix size of w to have the same size as the scoreboard
 	h = math.Min(h, ScrH()*0.95)
 	if #self.Items == 1 then -- It fucks up when there's only one icon in
 		h = math.Max(y or 0, 120)
 	end
-	
+
 	self:SetSize(w, h)
 	self:PerformLayout()
 end
@@ -47,13 +47,13 @@ function PANEL3:Init()
 	self.Header = vgui.Create("FAdminPlayerCatagoryHeader", self)
 	self.Header:SetTall(25)
 	self:SetPadding(5)
-	
+
 	self:SetExpanded(true)
 	self:SetMouseInputEnabled(true)
-	
+
 	self:SetAnimTime(0.2)
 	self.animSlide = Derma_Anim("Anim", self, self.AnimSlide)
-	
+
 	self:SetDrawBackground(true)
 
 end
@@ -67,140 +67,40 @@ end
 
 derma.DefineControl("FAdminPlayerCatagory", "DCatagoryCollapse adapted for FAdmin", PANEL3, "DCollapsibleCategory")
 
-
--- FAdminPlayerIcon
-local PANEL4 = {}
-
-function PANEL4:Init()
-	self:SetSize(96, 116)
-	self.Icon = vgui.Create("SpawnIcon", self)
-	self.Icon:SetMouseInputEnabled(false)
-	self.Icon:SetKeyboardInputEnabled(false)
-	
-	
-	self.animPress = Derma_Anim("Press", self.Icon, self.Icon.PressedAnim)
-	self.Icon:SetIconSize(96)
-	self.NameLabel = vgui.Create("DLabel", self)
-	self.NameLabel:SetText("PlayerName")
-	self.NameLabel:SetTextColor(Color(255, 255, 255, 250))
-	self.NameLabel:SetMouseInputEnabled(false)
-end
-
-function PANEL4:PerformLayout()
-	self.Icon:SetIconSize(94)
-	self.NameLabel:SetPos(10, self:GetTall() - self.NameLabel:GetTall())
-	self.NameLabel:SetWide(self:GetWide() - 20)
-	self.NameLabel:SetContentAlignment(5)
-	self.NameLabel:SetZPos(100)
-	
-	local size = self:GetTall() - self.NameLabel:GetTall()
-	self.Icon:SetSize(self:GetWide()-2, self:GetTall()-2)
-	self.Icon:SetPos(1, 1)
-
-end
-
-function PANEL4:OnMousePressed(mcode)
-
-	if (mcode == MOUSE_LEFT) then
-		self:DoClick()
-		self.animPress:Start(0.2)
-	end
-	
-	if (mcode == MOUSE_RIGHT) then
-		self:OpenMenu()
-	end
-
-end
-
-function PANEL4:DoClick(mcode)
-end
-
-function PANEL4:OnCursorEntered()
-	self:SetSize(96, 116)
-	self.PaintOverOld = self.PaintOver
-	self.PaintOver = self.PaintOverHovered
-end
-
-function PANEL4:OnCursorExited()
-	if (self.PaintOver == self.PaintOverHovered) then
-		self.PaintOver = self.PaintOverOld
-	end
-end
-
-function PANEL4:Think()
-	self.animPress:Run()
-end
-
-function PANEL4:PaintOverHovered()
-	self.Icon:PaintOverHovered()
-	surface.SetDrawColor(255, 155, 20, 255)
-	surface.DrawRect(0, 0, self:GetWide(), self:GetTall())
-	self.Icon:PaintManual()
-	self.NameLabel:PaintManual()
-end
-
-
-function PANEL4:Paint()
-	surface.SetDrawColor(255, 255, 255, 50)
-	
-	surface.DrawRect(0, 0, self:GetWide(), self:GetTall())
-end
-
-function PANEL4:SetPlayer(ply)
-	self.Player = ply
-	self.NameLabel:SetText(ply:Nick())
-	self.Icon:SetModel(ply:GetModel() or "models/player/Kleiner.mdl", ply:GetSkin())
-	self:SetToolTip(ply:Nick())
-end
-
-function PANEL4:OpenMenu()
-	if table.Count(FAdmin.PlayerIcon.RightClickOptions) < 1 then return end
-	local menu = DermaMenu()
-	
-	menu:SetPos(gui.MouseX(), gui.MouseY())
-	
-	for Name, func in pairs(FAdmin.PlayerIcon.RightClickOptions) do
-		menu:AddOption(Name, function() func(self.Player, self) end)
-	end
-	
-	menu:Open()
-end
-derma.DefineControl("FAdminPlayerIcon", "Icon for Player in scoreboard", PANEL4, "Panel")
-
 -- FAdmin player row (from the sandbox player row)
 PANEL = {}
 
 CreateClientConVar("FAdmin_PlayerRowSize", 30, true, false)
 function PANEL:Init()
 	self.Size = GetConVarNumber("FAdmin_PlayerRowSize")
-	
+
 	self.lblName 	= vgui.Create("Label", self)
 	self.lblFrags 	= vgui.Create("Label", self)
 	self.lblTeam	= vgui.Create("Label", self)
 	self.lblDeaths 	= vgui.Create("Label", self)
 	self.lblPing 	= vgui.Create("Label", self)
-	
+
 	// If you don't do this it'll block your clicks
 	self.lblName:SetMouseInputEnabled(false)
 	self.lblTeam:SetMouseInputEnabled(false)
 	self.lblFrags:SetMouseInputEnabled(false)
 	self.lblDeaths:SetMouseInputEnabled(false)
 	self.lblPing:SetMouseInputEnabled(false)
-	
+
 	self.imgAvatar = vgui.Create("AvatarImage", self)
-	
+
 	self:SetCursor("hand")
 end
 
 function PANEL:Paint()
 	if not ValidEntity(self.Player) then return end
-	
+
 	self.Size = GetConVarNumber("FAdmin_PlayerRowSize")
 	self.imgAvatar:SetSize(self.Size - 4, self.Size - 4)
-	
+
 	local color = Color(100, 150, 245, 255)
 
-	
+
 	if GAMEMODE.Name == "Sandbox" then
 		color = Color(100, 150, 245, 255)
 		if self.Player:Team() == TEAM_CONNECTING then
@@ -210,12 +110,12 @@ function PANEL:Paint()
 		end
 
 		if self.Player:GetFriendStatus() == "friend" then
-			color = Color(236, 181, 113, 255)	
+			color = Color(236, 181, 113, 255)
 		end
 	else
 		color = team.GetColor(self.Player:Team())
 	end
-	
+
 	local hooks = hook.GetTable().FAdmin_PlayerRowColour
 	if hooks then
 		for k,v in pairs(hooks) do
@@ -223,22 +123,22 @@ function PANEL:Paint()
 			break
 		end
 	end
-	
+
 	draw.RoundedBox(4, 0, 0, self:GetWide(), self.Size, color)
-	
+
 	surface.SetTexture(texGradient)
 	if self.Player == LocalPlayer() or self.Player:GetFriendStatus() == "friend" then
 		surface.SetDrawColor(255, 255, 255, 50 + math.sin(RealTime() * 2) * 50)
 	end
-	surface.DrawTexturedRect(0, 0, self:GetWide(), self.Size) 	
+	surface.DrawTexturedRect(0, 0, self:GetWide(), self.Size)
 	return true
 end
 
 function PANEL:SetPlayer(ply)
 	self.Player = ply
-	
+
 	self.imgAvatar:SetPlayer(ply)
-	
+
 	self:UpdatePlayerData()
 end
 
@@ -260,7 +160,7 @@ function PANEL:ApplySchemeSettings()
 	self.lblFrags:SetFont("ScoreboardPlayerName")
 	self.lblDeaths:SetFont("ScoreboardPlayerName")
 	self.lblPing:SetFont("ScoreboardPlayerName")
-	
+
 	self.lblName:SetFGColor(color_white)
 	self.lblTeam:SetFGColor(color_white)
 	self.lblFrags:SetFGColor(color_white)
@@ -276,13 +176,13 @@ end
 function PANEL:DoRightClick()
 	if table.Count(FAdmin.PlayerIcon.RightClickOptions) < 1 then return end
 	local menu = DermaMenu()
-	
+
 	menu:SetPos(gui.MouseX(), gui.MouseY())
-	
+
 	for Name, func in pairs(FAdmin.PlayerIcon.RightClickOptions) do
 		menu:AddOption(Name, function() func(self.Player, self) end)
 	end
-	
+
 	menu:Open()
 end
 
@@ -298,17 +198,17 @@ function PANEL:PerformLayout()
 	self.imgAvatar:SetSize(32, 32)
 
 	self:SetSize(self:GetWide(), self.Size)
-	
+
 	self.lblName:SizeToContents()
 	self.lblName:SetPos(24, 2)
 	self.lblName:MoveRightOf(self.imgAvatar, 8)
-	
+
 	local COLUMN_SIZE = 75
-	
+
 	self.lblPing:SetPos(self:GetWide() - COLUMN_SIZE * 0.4, 0)
 	self.lblDeaths:SetPos(self:GetWide() - COLUMN_SIZE * 1.4, 0)
 	self.lblFrags:SetPos(self:GetWide() - COLUMN_SIZE * 2.4, 0)
-	
+
 	self.lblTeam:SetPos(self:GetWide() / 2 - (0.5*self.lblTeam:GetWide()))
 end
 vgui.Register("FadminPlayerRow", PANEL, "Button")
@@ -321,12 +221,12 @@ function PANEL6:Init()
 	self:SetDrawBorder(false)
 	self:SetStretchToFit(false)
 	self:SetSize(120, 40)
-	
+
 	self.TextLabel = vgui.Create("DLabel", self)
 	self.TextLabel:SetFont("ChatFont")
-	
+
 	self.m_Image2 = vgui.Create("DImage", self)
-	
+
 	self.BorderColor = Color(190,40,0,255)
 end
 
@@ -340,10 +240,10 @@ end
 function PANEL6:PerformLayout()
 	self.m_Image:SetSize(32,32)
 	self.m_Image:SetPos(4,4)
-	
+
 	self.m_Image2:SetSize(32, 32)
 	self.m_Image2:SetPos(4,4)
-	
+
 	self.TextLabel:SetPos(38, 8)
 end
 
