e0a63065210acd6f5955c6898f4a3e59aad770a2
fpeijnenburg@gmail.com <fpeijnenburg@gmail.com@57cffa9e-d0df-11dd-9aad-3f7dfcff7c77>
2012-07-31 19:38:35 +0000

    Chat bug is either fixed or doesn't exist.

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -451,7 +451,7 @@ local function AddToChat(msg)
 	local name = msg:ReadString()
 	local ply = msg:ReadEntity() or LocalPlayer()
 
-	if name == "" then
+	if name == "" or not name then
 		name = ply:Nick()
 		name = name ~= "" and name or ply:SteamName()
 	end
